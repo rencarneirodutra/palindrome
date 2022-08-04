f201a15daddcd14fd4d9056b056c636033f4f94a
Renan Carneiro <rencarneirodutra@gmail.com>
2022-08-04 11:36:47 -0300

    checking if the string is a palindrome

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindrome
+{
+     class Program
+    {
+
+        static void Main(string[] args)
+        {
+            resultado();
+        }
+
+        //Função de interação com o usuário
+        public static void resultado()
+        {
+            Console.WriteLine("Digite algo: ");
+            string palindromo = Console.ReadLine();
+
+            bool res = checarPalindromo(palindromo);
+
+            Console.WriteLine(res);
+
+        }
+
+        // Nesta função estamos usando o método Reverse() para inverter nossa string e compará-la com a string original usando o método SequenceEqual(). 
+        public static bool checarPalindromo(string palindromo)
+        {
+            return palindromo.SequenceEqual(palindromo.Reverse());
+        }
+
+
+    }
+}
